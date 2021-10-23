4875f696764cd6103683a32d9eb543755b4edc5f
эва <evelina.shatunova@urfu.me>
2021-10-24 00:53:35 +0500

    выаолнено задание2

---
+++ task2(28)/task2(28)/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2_28_
+{
+    class Program
+
+    {
+        public static int AriphmeticProgress(int memb, int dif, int n)
+        {
+            if (n > 0)
+            { memb = dif + AriphmeticProgress(memb, dif, n - 1); }
+            return memb;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите N");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите разность");
+            int differ = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите первый член прогрессии");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ответ = {0}", AriphmeticProgress(first, differ, a - 1));
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
