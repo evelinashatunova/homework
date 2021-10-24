91a1ee9054138626af40ccceed873225ee4767a3
эва <evelina.shatunova@urfu.me>
2021-10-24 22:05:03 +0500

    выполнено задание3

---
+++ task3(28)/task3(28)/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3_28_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите x");
+            double y;
+            Console.WriteLine(value: y = conclusion);
+            Console.ReadKey();
+
+        }
+       
+        static double conclusion
+        {
+            get
+
+            {
+                var x = double.Parse(Console.ReadLine());
+                double y;
+                return y = Math.Atan((1 + (Math.Pow(x, 2)) / 2));
+            }
+
+        }
+    }
+}
