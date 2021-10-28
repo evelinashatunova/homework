16d36aa4717af9d903359aa3aca461a54972cac6
эва <evelina.shatunova@urfu.me>
2021-10-28 14:00:30 +0500

    выполнено задание5

---
+++ classwork2/classwork2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classwork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите координату x");
+            var x = double.Parse(Console.ReadLine());
+            Console.WriteLine("введите координату y");
+            var y = double.Parse(Console.ReadLine());
+            
+            if (x == 0)
+            {
+                if (y == 0)
+                {
+                    Console.WriteLine($"точка совпадает с началом координат");
+                }
+                else
+                {
+                    Console.WriteLine($"njxrf({x};{y})лежит на оси oy");
+       
+                }
+            }
+            else
+            {
+                if (y == 0)
+
+                {
+                    Console.WriteLine($"точка({x};{y}) лежит на оси ox");
+                }
+                else
+                {
+                    if (x>0)
+                    {
+                        if(y>0)
+                        {
+                            Console.WriteLine($"точка({x};{y}) лежит в I квандранте");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"точка ({x};{y}) лежит в IV квадранте");
+
+                        }
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ task5(28)/task5(28)/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task5_28_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите текст на английском языке");
+            var text = Console.ReadLine();
+            text = text.ToUpperInvariant();
+            Console.WriteLine($"транслитерация текста:" + TransliterateText( text));
+            Console.ReadKey();
+        }
+        static string TransliterateText(string  text)
+        {
+            text = text.Replace("A", "4");
+            text = text.Replace("B", "8");
+            text = text.Replace("C", "(");
+            text = text.Replace("D", "|)");
+            text = text.Replace("E", "3");
+            text = text.Replace("F", "|=");
+            text = text.Replace("G", "6");
+            text = text.Replace("H", "|-|");
+            text = text.Replace("I", "!");
+            text = text.Replace ("J", ")");
+            text = text.Replace ("K", "|<");
+            text = text.Replace("L", "1");
+            text = text.Replace("M", "|V|");
+            text = text.Replace("N", "|/|");
+            text = text.Replace("O", "0");
+            text = text.Replace("P", "|>");
+            text = text.Replace("Q", "R");
+            text = text.Replace("R", "|2");
+            text = text.Replace("S", "5");
+            text = text.Replace("T", "7");
+            text = text.Replace("U", "|_|");
+            text = text.Replace("V", "/");
+            text = text.Replace("W", "VV");
+            text = text.Replace("X", "><");
+            text = text.Replace("Y", "'/");
+            text = text.Replace("Z", "2");
+            return text;
+
+
+
+
+        }
+    }
+}
