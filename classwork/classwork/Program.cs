9e4b7b93ba10759438607bf18816f4b2c5bad9cc
эва <evelina.shatunova@urfu.me>
2021-12-17 01:03:25 +0500

    выполнено задание 7

---
+++ classwork/classwork/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите начальное значение аргумента");
+            var x = double.Parse(Console.ReadLine());
+            Console.WriteLine("введите значение шага");
+            var d = double.Parse(Console.ReadLine());
+            Console.WriteLine("введите число строк таблицы");
+            var n = double.Parse(Console.ReadLine());
+            Console.WriteLine(" x | x^2");
+            Console.WriteLine(new string('-', 15));
+
+            for (var i = 0; i < n; i++) 
+            {
+                Console.WriteLine($"{x,5:F2} | {x * x:F3}");
+                x += d;
+            }
+            
+            
+            
+            
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
