f44626e6f57f6033aeee596ddd550b8ba714eb6e
эва <evelina.shatunova@urfu.me>
2021-12-28 21:32:40 +0500

    выполнено задание 6.2

---
+++ task6.2(28)/task6.2(28)/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task6._2_28_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координату x");
+            var x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите координату y");
+            var y = double.Parse(Console.ReadLine());
+
+            GetCoordinate();
+            Console.ReadKey();
+           
+             void GetCoordinate()
+            {
+                
+                if (x >= -1 && y >= -2)
+                {
+                    Console.WriteLine($"Точка ({x};{y}) принадлежит к указанной области");
+                }
+                else if (x <= -3 && y >= -2)
+                {
+                    Console.WriteLine($"Точка ({x};{y}) принадлежит к указанной области");
+                }
+                else
+                {
+                    Console.WriteLine($"Точка ({x};{y}) не принадлежит к указанной области");
+                }
+                
+            }      
+        }
+    }
+}
---
+++ task8.2(28)/task8.2(28)/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task8._2_28_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal sum = 0;
+            Console.WriteLine("Введите количество товаров");
+            int amount = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите стоимость товаров");
+            decimal price = decimal.Parse(Console.ReadLine());
+
+            for (int i = 0; i >=1; i++) 
+            {
+                Console.WriteLine(sum = amount * price);
+
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
