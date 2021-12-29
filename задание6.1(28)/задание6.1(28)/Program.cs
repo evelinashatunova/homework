5f0f27e19d297785440e9ef6b6b3ea49995a270d
эва <evelina.shatunova@urfu.me>
2021-12-29 17:19:02 +0500

    выполнено задание 6.1

---
+++ задание6.1(28)/задание6.1(28)/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace задание6._1_28_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var m = int.Parse(Console.ReadLine());
+            var n = int.Parse(Console.ReadLine());
+            Console.WriteLine(logic());
+            Console.ReadKey();
+
+            bool logic()
+            {
+                return (m / m == m) || (n / n == n);
+
+            }
+        }
+    }
+}
