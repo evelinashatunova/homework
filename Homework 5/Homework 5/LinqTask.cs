c31ce225a273ba9d5036862c872165708e1b06ae
эва <evelina.shatunova@urfu.me>
2023-06-12 01:03:40 +0500

    задание5

---
+++ Homework 5/Homework 5/LinqTask.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Homework_5
+{
+    public class LinqTask
+    {
+        public string[] GetWordsArray(string[] lines)
+        {
+            return lines
+                .Select(line => line.Split())
+                .SelectMany(words => words)
+                .Where(word => Char.IsUpper(word[0]))
+                .OrderBy(word => word)
+                .ToArray();
+        }
+    }
+}
---
+++ Homework 5/Homework 5/Record.cs
@@ -0,0 +1,10 @@
+﻿namespace Homework_5
+{
+    public struct Record
+    {
+        public int ClientID;
+        public int Year;
+        public int Month;
+        public int Duration;
+    }
+}
